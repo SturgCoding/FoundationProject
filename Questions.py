001e6407b872a271eb755005dd2f03a4b1f7573b
Sturgess <sam.sturgess@btinternet.com>
2026-02-08 14:09:03 +0000

    Changed Bool update logic and fixed code to update the text values on the Answers and the Question heading

--- Questions.py
+++ Questions.py
@@ -4,13 +4,7 @@ def question_no(question=0):
     Procedure: Find the question from question number
     Defaults: question = 0 else passed val, question_text = "" blank if question fails
     """
-    question_text = "" #Default to blank
-    try:
-        question_text = find_question(question)
-    except TypeError:
-        pass
-    finally:
-        return question_text
+    pass
 
 def find_question(question):
     QUESTION_LIST = ["What is the formula for Voltage?","N/A","N/A"]
--- TkTemplate.py
+++ TkTemplate.py
@@ -3,7 +3,6 @@
     # ==========================================
 
 import tkinter as tk
-from tkinter import ttk
 
 #import abstracted code/ideas
     #E.g. Question handling code
@@ -56,10 +55,11 @@ class Q_Page:
         # Question_Area
         self.Question_Area = tk.Message(
             self.root,
-            font=("Courier New", 15),
-            textvariable = "EXAMPLE CODE",
+            font=("Courier New", 12),
+            text = "Initialized...",
             bg="#ffffff",
-            fg="#000000"
+            fg="#000000",
+            width=690 # Message needs width of box to wrap text correctly
         )
         self.Question_Area.place(x=60, y=110, width=690, height=120)
 
@@ -77,34 +77,31 @@ class Q_Page:
         # A_1
         self.A_1 = tk.Checkbutton(
             self.root,
-            text="{Ans}",
+            text="A_1 Initialized",
             variable=self.Ans1,
             command=self.on_A_1_click
         )
         self.A_1.place(x=380, y=260)
-
         # A_2
         self.A_2 = tk.Checkbutton(
             self.root,
-            text="{Ans}",
+            text="A_2 Initialized",
             variable=self.Ans2,
             command=self.on_A_2_click
         )
         self.A_2.place(x=380, y=300)
-
         # A_3
         self.A_3 = tk.Checkbutton(
             self.root,
-            text="{Ans}",
+            text="A_3 Initialized",
             variable=self.Ans3,
             command=self.on_A_3_click
         )
         self.A_3.place(x=380, y=340)
-
         # A_4
         self.A_4 = tk.Checkbutton(
             self.root,
-            text="{Ans}",
+            text="A_4 Initialized",
             variable=self.Ans4,
             command=self.on_A_4_click
         )
@@ -135,24 +132,24 @@ class Q_Page:
     # Get/Set Handlers - Implement change
     # ==========================================
     def get_Ans1(self):
-        return self.Ans1
+        return self.Ans1.get()
     def set_Ans1(self, bool):
-        self.Ans1 = bool
+        self.Ans1.set(bool)
 
     def get_Ans2(self):
-        return self.Ans2
+        return self.Ans2.get()
     def set_Ans2(self, bool):
-        self.Ans2 = bool
+        self.Ans2.set(bool)
 
     def get_Ans3(self):
-        return self.Ans3
+        return self.Ans3.get()
     def set_Ans3(self, bool):
-        self.Ans3 = bool
+        self.Ans3.set(bool)
 
     def get_Ans4(self):
-        return self.Ans3
+        return self.Ans4.get()
     def set_Ans4(self, bool):
-        self.Ans4 = bool
+        self.Ans4.set(bool)
 
 #-----------------------------------------------------------------
     # ==========================================
@@ -162,59 +159,74 @@ class Q_Page:
     def on_Quit_click(self):
         """
         Handle on_Quit_click event
-        Procedure: Exit program
+        Procedure: Exit/Close program
         """
         self.root.destroy()
 
+    # No additional code necessarily needed for clicks
+        # Avoids double assignment then
     def on_A_1_click(self):
         """
         Handle on_A_1_click event
         """
-        self.Ans1 = not self.Ans1
-
+        pass
     def on_A_2_click(self):
         """
         Handle on_A_2_click event
         """
-        self.Ans2 = not self.Ans2
-
+        pass
     def on_A_3_click(self):
         """
         Handle on_A_3_click event
         """
-        self.Ans3 = not self.Ans3
-
+        pass
     def on_A_4_click(self):
         """
         Handle on_A_4_click event
         """
-        self.Ans4 = not self.Ans4
+        pass
 
     def on_Clear_Ans_click(self):
         """
         Handle on_Clear_Ans_click event
         Function: Reset all selected answers to unchecked answers
         """
-        Q_Page.set_Ans1(self, False)
-        Q_Page.set_Ans2(self, False)
-        Q_Page.set_Ans3(self, False)
-        Q_Page.set_Ans4(self, False)
+        self.set_Ans1(False)
+        self.set_Ans2(False)
+        self.set_Ans3(False)
+        self.set_Ans4(False)
 
     def on_Submit_click(self):
         """
         Handle on_Submit_click event
         TODO: Implement code here
         """
+        # Output all states of answers
+        print(f"A1:{self.get_Ans1()}, A2:{self.get_Ans2()}, A3:{self.get_Ans3()}, A4:{self.get_Ans4()}")
 
 #-----------------------------------------------------------------
     # ==========================================
-    # Event Handlers - Implement event logic
+    # Update Handlers - Implement direct logic for updates
     # ==========================================
+    def update_question(self, new_Question):
+        self.Question_Area.config(text=new_Question)
 
+    def update_Ans1_Text(self, Ans1_Text):
+        self.A_1.config(text=Ans1_Text)
+
+    def update_Ans2_Text(self, Ans2_Text):
+        self.A_2.config(text=Ans2_Text)
+
+    def update_Ans3_Text(self, Ans3_Text):
+        self.A_3.config(text=Ans3_Text)
+
+    def update_Ans4_Text(self, Ans4_Text):
+        self.A_4.config(text=Ans4_Text)
 
 #-----------------------------------------------------------------
 
 if __name__ == "__main__":
+    print(f"TKinter Version {tk.TkVersion}")
     root = tk.Tk()
     app = Q_Page(root)
     root.mainloop()
