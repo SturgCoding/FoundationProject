7a2aa811987bc3296e38d577fd25b723c7632582
Gabriel <gabrielstahurschi@gmail.com>
2026-03-04 13:21:35 +0000

    main

--- src/simulation.py
+++ src/simulation.py
@@ -8,8 +8,7 @@
 
 
 import pygame
-import pygame.surface
-import pygame_gui
+
 import sys
   
 pygame.init()
@@ -27,27 +26,12 @@ RED = (255, 50, 50)
 #pygame clock
 clock = pygame.time.Clock()
 
-ui_manager = pygame_gui.UIManager((SCREEN_WIDTH, SCREEN_HEIGHT - 50))
 
-#slider for the mass
-mass_slider = pygame_gui.elements.UIHorizontalSlider(
-    relative_rect=pygame.Rect((50, 50), (200, 20)),
-    start_value=1.0,
-    value_range=(0.1, 10.0),
-    manager=ui_manager
-)
-
-#label for the mass slider
-mass_label = pygame_gui.elements.UILabel(
-    relative_rect=pygame.Rect((50, 20), (200, 20)),
-    text="Mass:",
-    manager=ui_manager
-)
 
 
 class BeanCan:
     def __init__(self,x ,y):
-        self.original_image = pygame.image.load("bean_can.png").convert_alpha()
+        self.original_image = pygame.image.load("assets/bean_can.png").convert_alpha()
         self.image = pygame.transform.scale(self.original_image, (60, 100))
         self.rect = self.image.get_rect()
         self.rect.x = x
@@ -90,20 +74,10 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        ui_manager.process_events(event)
+        
         bean.handle_event(event)
-
-     #checking if the slider moved
-    ui_manager.update(time_delta)
-    
-    current_mass = mass_slider.get_current_value()
-    bean.mass = current_mass
-
-    mass_label.set_text(f"Mass: {current_mass:.1f} kg")
-
     screen.fill(WHITE)
     bean.draw(screen)
-    ui_manager.draw_ui(screen)
     pygame.display.flip()
 
 pygame.quit()
